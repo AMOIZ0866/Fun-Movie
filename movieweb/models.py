d2ca747632921b27dfa12145e06ec215283b3b39
moiz <abdul.moiz@phaedrasolutions.com>
2021-11-08 12:37:54 +0500

    model changes

--- movieweb/models.py
+++ movieweb/models.py
@@ -1,18 +1,4 @@
 from django.db import models
-from django.db.models.fields import BooleanField
-
-
-# Create your models here.
-
-class Signup(models.Model):
-    username = models.CharField(max_length=20, unique=True)
-    password = models.CharField(max_length=20,)
-    email = models.CharField(max_length=50, unique=True)
-    type = models.BooleanField(default=False)
-    active = models.BooleanField(default=True)
-    last_login = models.DateTimeField()
-    creation_date = models.DateTimeField()
-    update_date = models.DateTimeField()
 
 
 class Movie(models.Model):
@@ -22,11 +8,11 @@ class Movie(models.Model):
     review = models.CharField(max_length=250, null=True)
     vurl = models.CharField(max_length=250)
     imageurl = models.CharField(max_length=250)
-    # mrating = models.FloatField(max_length=20)
     creation_date = models.DateTimeField()
     update_date = models.DateTimeField()
 
+
 class Rating(models.Model):
     mname = models.ForeignKey(Movie, related_name='rating', on_delete=models.CASCADE)
-    mrating = models.FloatField(max_length=20)
-    noofupdate=models.IntegerField(max_length=250)
+    mrating = models.FloatField()
+    noofupdate = models.IntegerField()
